683296c459cbea70ed0b34ed959f24bb437caf7e
jiropardo <jairo.prado@databricks.com>
2025-01-09 21:51:31 -0600

    adding files

---
+++ modules/module1.py
@@ -0,0 +1,13 @@
+class MyClass:
+    def __init__(self):
+        print("MyClass instance created")
+
+    def method_ONE():
+        return "Class function"    
+
+def function1():
+        return "Hello from module1!"
+
+
+
+
---
+++ modules/module2.py
@@ -0,0 +1,4 @@
+from .module1 import function1
+
+def function2():
+    return f"module2 says: {function1()}"
